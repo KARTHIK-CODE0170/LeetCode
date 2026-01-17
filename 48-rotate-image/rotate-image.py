e7d64306b29924bb7fe72f011f13abf37d51e8cf
Karthik P <pandirikarthik5@gmail.com>
2026-01-17 21:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (17.33%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -6,7 +6,7 @@ class Solution:
         n = len(matrix)
         for i in range(n - 1):
             for j in range(i+1,n):
-                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]#swap(matrix[i][j],matrix[j][i])
         for i in range(n):
             matrix[i]= matrix[i][::-1]
         return matrix
