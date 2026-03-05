caca73a01373b6be62f78d021f3753b4bf991521
Karthik P <pandirikarthik5@gmail.com>
2026-03-05 20:00:10 +0530

    Time: 9 ms (47.81%) | Memory: 19.2 MB (93.82%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -5,4 +5,8 @@ class Solution:
         for i in range(x):
             if((i % 2 == 0 and s[i] == '1') or (i % 2 != 0 and s[i] == '0')):
                One += 1
+        # We find the difference of one pattern, in this case we found the with '10101010...' series
+
         return min(One,x - One)
+        #The return is done for the optimal solution as we found the difference of one series the another one will be 
+        #return (Total length of that minus the current difference)
