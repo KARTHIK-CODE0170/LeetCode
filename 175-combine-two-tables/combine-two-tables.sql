fa90d67f704ec0e28d786c76010c7eb4b3ed939e
Karthik P <pandirikarthik5@gmail.com>
2026-03-13 20:14:14 +0530

    Time: 440 ms (85.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,12 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM
+person p
+LEFT OUTER JOIN
+address a
+ON 
+p.personId = a.personId;
