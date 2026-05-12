76889c5d370c508de7b24a59a64885aa5b5833ce
Karthik P <pandirikarthik5@gmail.com>
2026-05-12 12:50:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (66.06%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -4,13 +4,22 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
+        cnt = 0
+        if len(nums) == 1:
+            return 
+        #find the decrease in from the right side
         i = n - 2
-        while(i >= 0 and nums[i] >= nums[i+1]):
+        while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
-        if(i >= 0):
+        if i >= 0:
             j = n - 1
-            while(j >= 0 and nums[j] <= nums[i]):
+            while nums[j] <= nums[i] and j >= 0:
                 j -= 1
-            nums[i] , nums[j] = nums[j], nums[i]
-        nums[i+ 1:] = reversed(nums[i+1:])
+            nums[i],nums[j] = nums[j],nums[i]
+        nums[i + 1:] = reversed(nums[i+ 1:])
         return nums
+        
+
+        
+
+
