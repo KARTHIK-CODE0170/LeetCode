627e1b490fa44f37e763305b47d8a2b9d20d9d49
Karthik P <pandirikarthik5@gmail.com>
2026-07-13 20:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.48%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,45 +1,30 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        m, n = len(matrix),len(matrix[0])
-        res = list()
-        top,bottom = 0,m
-        left,right = 0,n
-        #We are having 4loops for the traversal of the all the 4stages in the loop
-        while(top < bottom and left < right):
-            #left to right
-            l = left
-            while(l < right):
-                res.append(matrix[top][l])
-                l += 1
+    def spiralOrder(self, arr: List[List[int]]) -> List[int]:
+        left = top = 0
+        right = len(arr[0]) - 1
+        down = len(arr) - 1
+        res = []
+        while left <= right and top <= down:
+            i = left 
+            while i <= right:   
+                res.append(arr[top][i])
+                i += 1
             top += 1
-
-            #top to bottom
-            u = top
-            while(u < bottom):
-                res.append(matrix[u][right - 1])
-                u += 1
+            i = top
+            while i <= down:
+                res.append(arr[i][right])
+                i += 1
             right -= 1
-            
-            #right to left
-            if top < bottom:
-                r = right - 1
-                while(r >= left):
-                    res.append(matrix[bottom-1][r])
-                    r -= 1
-                bottom -= 1
-
-            #bottom to top
-            if left < right: 
-                b = bottom - 1
-                while(b >= top):
-                    res.append(matrix[b][left])
-                    b -= 1
+            i = right
+            if top <= down:
+                while i >= left:
+                    res.append(arr[down][i])
+                    i -= 1
+                down -= 1
+            i = down
+            if left <= right:
+                while i >= top:
+                    res.append(arr[i][left])
+                    i -= 1
                 left += 1
         return res
-
-
-
-
-            
-            
-                
