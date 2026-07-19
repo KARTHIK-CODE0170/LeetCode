08c0b79d8ea6b6535860ef72bb855627e8ef820a
Karthik P <pandirikarthik5@gmail.com>
2026-07-19 21:36:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (77.50%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,11 +1,24 @@
 class Solution:
+    def fun(self,arr,low,high,tar):
+        while low <= high:
+            mid = (high + low) //2
+            if arr[mid] == tar:
+                return True
+            elif arr[mid] < tar:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
     def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
         up, down = 0 , len(mat) - 1
-        n = len(mat[0]) -1
+        n = len(mat[0])
         while up <= down:
             mid = (up + down) // 2
-            if target in mat[mid]:
-                return True
+            if mat[mid][0] <= target <= mat[mid][n - 1]:
+                if self.fun(mat[mid],0,n-1,target):
+                    return True
+                else:
+                    return False
             elif target < mat[mid][0]:
                 down = mid - 1
             else:
