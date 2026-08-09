ba05f38f052d9717707b2a55b721e554ed48a06a
Karthik P <pandirikarthik5@gmail.com>
2026-08-09 18:50:28 +0530

    Time: 2 ms (72.96%) | Memory: 19.5 MB (8.26%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -1,16 +1,18 @@
+from collections import deque
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        result = []
-        balance = 0
-        
-        for char in s:
-            if char == '(':
-                if balance > 0:
-                    result.append(char)
-                balance += 1
+        arr = []
+        flag = 0
+        for i in s:
+            if flag == 0 and i == "(":
+                flag += 1
+            elif i == "(":
+                arr.append(i)
+                flag += 1
+            elif i == ")" and flag - 1 == 0:
+                # arr.append(i)
+                flag -= 1
             else:
-                balance -= 1
-                if balance > 0:
-                    result.append(char)
-                    
-        return "".join(result)
+                arr.append(i)
+                flag -= 1
+        return ''.join(arr)
