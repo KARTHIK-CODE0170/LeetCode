cb56527f1a33fda22be52ff50ad1514657a8ce93
Karthik P <pandirikarthik5@gmail.com>
2026-08-11 21:19:19 +0530

    Time: 203 ms (94.48%) | Memory: 29.9 MB (90.09%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -1,19 +1,26 @@
+MAX_VAL = 10**6
+arr = [True] * (MAX_VAL + 1)
+arr[0] = arr[1] = False
+i = 2
+while i * i <= MAX_VAL:
+    for j in range(i * i,MAX_VAL+1,i):
+        arr[j] = False
+    i += 1
 class Solution:
-    def god(self,right):
-        # right += 1
-        arr = [True] * (right + 1)
-        arr[0] = arr[1] = False
-        i = 2
-        while i * i <= right:
-            for j in range(i * i,right+1,i):
-                arr[j] = False
-            i += 1
-        return arr
+    # def god(self,right):
+    #     # right += 1
+    #     arr = [True] * (right + 1)
+    #     arr[0] = arr[1] = False
+    #     i = 2
+    #     while i * i <= right:
+    #         for j in range(i * i,right+1,i):
+    #             arr[j] = False
+    #         i += 1
+        
 
 
     def closestPrimes(self, left: int, right: int) -> List[int]:
         res = []
-        arr = self.god(right)
         for i in range(left,right + 1):
             if arr[i]:
                 res.append(i)
@@ -22,11 +29,10 @@ class Solution:
         ans = [-1,-1]
         while i < len(res):
             min_val = res[i] - res[i - 1]
-            if (min_val) == min_diff:
-                i += 1
-                continue
             if (min_val) < min_diff:
                 ans[0],ans[1]  = res[i-1],res[i]
                 min_diff = min_val
+            if min_diff == 2:
+                break
             i += 1
         return ans
