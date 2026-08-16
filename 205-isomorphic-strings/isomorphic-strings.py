6485361d7e73822100c41479d06e82b13321d576
Karthik P <pandirikarthik5@gmail.com>
2026-08-16 12:24:33 +0530

    Time: 3 ms (91.47%) | Memory: 19.2 MB (47.38%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,13 +2,10 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        freq = dict()
+        st,ts = {},{}
         for i,j in zip(s,t):
-            x = freq.get(i,0)
-            if x == 0 and j not in freq.values():
-                freq[i] = j
-            elif x != j:
+            if (i in st and st[i] != j) or (j in ts and ts[j] !=i):
                 return False
+            st[i] = j
+            ts[j] = i
         return True
-
-        
