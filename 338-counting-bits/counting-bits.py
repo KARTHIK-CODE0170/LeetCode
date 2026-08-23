e1b862e2db900f9778c7a1d5b6796366e2b85732
Karthik P <pandirikarthik5@gmail.com>
2026-08-23 13:00:59 +0530

    Time: 3 ms (95.33%) | Memory: 20.3 MB (18.24%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,14 +1,6 @@
 class Solution:
-    def check(slef,n):
-        cnt = 0
-        i = 1
-        while i <(n + 1):
-            if (n & i):
-                cnt += 1
-            i = i << 1
-        return cnt
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for i in range(n + 1):
-            ans.append(self.check(i))
-        return ans
+        res = [0] *  (n + 1)
+        for i in range(1,n + 1):
+            res[i] = res[i & (i - 1)] + 1
+        return res
