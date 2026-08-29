aaca0a15440d7771699087af3786bc3f9adb4bc5
Karthik P <pandirikarthik5@gmail.com>
2026-08-29 14:45:55 +0530

    Time: 278 ms (53.07%) | Memory: 19.1 MB (91.80%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,27 +1,29 @@
 class Solution:
-    def check(slef,s,left,right,arr):
-        maxi = 1
-        while left >= 0 and right < len(s):
+    def check(self,left,right,arr,s):
+        maxi = 0
+        while left > -1 and right < len(s):
             if s[left] != s[right]:
                 break
-            maxi = max(right - left + 1,maxi)
+            maxi =( right - left + 1)
             left -= 1
             right += 1
-            
-        arr[0],arr[1],arr[2] = left+1,right-1,maxi
-        return None
+        arr[0],arr[1],arr[2] = left+1,right - 1,maxi
+        
     def longestPalindrome(self, s: str) -> str:
-        left = right = 0
+        right = 0
         res = ""
-        arr = [0] * 3
         maxi = 0
-        for i in range(len(s)):
-            self.check(s,i,i,arr)
+        arr = [0] * 3
+        while right < len(s):
+            self.check(right,right,arr,s)
+            if arr[2] > maxi:
+                maxi = arr[2]
+                res = s[arr[0]:(arr[0] + arr[2])]
+            self.check(right,right + 1,arr,s)
             if arr[2] > maxi:
                 maxi = arr[2]
                 res = s[arr[0]:arr[0] + arr[2]]
-            self.check(s,i,i + 1,arr)
-            if arr[2] > maxi:
-                maxi = arr[2]
-                res = s[arr[0]:arr[0] + arr[2]]
+            right += 1
         return res
+            
+            
